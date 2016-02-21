778989991d05739982675f7baf73b39a65a1f344
Sarah <sarah.alspaugh@hotmail.com>
2016-02-20 21:09:25 -0800

    Added comments into the code

--- src/ArrayStas.java
+++ src/ArrayStas.java
@@ -1,10 +1,22 @@
+/**The ArrayStas Class generates 300 random numbers from 1-100. The class then sorts and displays the 
+ * numbers. The number of each is displayed for numbers 1-20, 21-40, 41-60, 61-80, 81-100.
+ * 
+ * @author Sarah Alspaugh
+ * @version 1
+ */
+
+//Import Random Class
 import java.util.Random;
 
 public class ArrayStas {
 	
 	private int[] numbers;
 	private int number;
-
+	
+	/**The Sort method checks the array for an empty array and if it is not empty is calls the quicksort
+	 * 
+	 * @param values	an array of integers
+	 */
 	public void sort(int[] values) {
 		// check for empty array
 		if (values ==null || values.length==0){
@@ -14,7 +26,12 @@ public class ArrayStas {
 		number = values.length;
 		quicksort(0, number - 1);
 	}
-
+	
+	/**The quicksort method takes the array of integers and sorts it using a pivot method
+	 * 
+	 * @param low	the lowest index
+	 * @param high	the highest index
+	 */
 	private void quicksort(int low, int high) {
 		int i = low, j = high;
 		// Get the pivot element from the middle of the list
@@ -46,12 +63,22 @@ public class ArrayStas {
 		     quicksort(i, high);
 		 }
 	}
-	private void swap(int i, int j) {
-		int temp = numbers[i];
-		numbers[i] = numbers[j];
-		numbers[j] = temp;
+	
+	/**
+	 * 
+	 * @param i
+	 * @param j
+	 */
+	private void swap(int number1, int number2) {
+		int temp = numbers[number1];
+		numbers[number1] = numbers[number2];
+		numbers[number2] = temp;
 		}
 	
+	/** The Main method creates 300 random numbers in an array. The array is sorted and displayed. 
+	 * The number of each value appearing in one of the five categories is then displayed. 
+	 * 
+	 */
 	public static void main(String[] args){
 		ArrayStas arrayMethods = new ArrayStas();
 		Random rand = new Random();
@@ -63,15 +90,15 @@ public class ArrayStas {
 		}
 		arrayMethods.sort(array);
 		for (int index = 0; index < 300; index++){
-
-				System.out.print(array[index] + "  ");
-				if (index == 20 || index == 40 || index == 60 || index == 80
-						|| index == 100 || index == 120 || index == 140 
-						|| index == 160 || index == 180 || index == 200
-						|| index == 220 || index == 240 || index == 260
-						|| index == 280 ){
+				System.out.print(array[index] + "   ");
+				if (index == 19 || index == 39 || index == 59 || index == 79
+						|| index == 99 || index == 119 || index == 139 
+						|| index == 159 || index == 179 || index == 199
+						|| index == 219 || index == 239 || index == 259
+						|| index == 279 ){
 					System.out.println();
 				}
-		} 
+		}
+		
 	}
 }
